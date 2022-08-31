64edcd499f96d21b62982ccdbd7176e5bd8f1e1a
fareedawan1 <ghulam05fareed@gmail.com>
2022-08-31 19:04:02 +0500

    Rename the registration_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-class RegistrationController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
+
+  def new
+    super
+  end
 
   # POST /resource
   # def create
