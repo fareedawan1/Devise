e5def838edf8795a8ed5a1654d64c385b8144345
fareedawan1 <ghulam05fareed@gmail.com>
2022-09-01 13:05:02 +0500

    add post model and posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
   root 'welcome#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
