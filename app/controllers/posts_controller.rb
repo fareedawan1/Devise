37ba73288f0993e12262c372e11154296f39531d
fareedawan1 <ghulam05fareed@gmail.com>
2022-09-01 13:23:33 +0500

    makes posts_controller workable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.order("created_at DESC ")
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -15,8 +23,25 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
+
+  private
+  
+  def find_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:title,:body)
+  end
+
 end
